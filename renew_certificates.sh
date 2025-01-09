2b0d7544c502cdd26af84441dbdf0bf5d58f9c87
Jônatas Tovani <98124177+jonatastovani@users.noreply.github.com>
2025-01-08 22:21:19 -0300

    Alterações para certbot

--- renew_certificates.sh
+++ renew_certificates.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-while :; do
+while true; do
     certbot renew --quiet --webroot --webroot-path=/var/www/app/public
-    sleep 5184000 # Espera 60 dias
+    sleep 5184000 # 60 dias em segundos
 done
